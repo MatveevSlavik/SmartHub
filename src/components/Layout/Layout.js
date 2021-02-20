4639c64bc7fca271665b8e67b33988a1678ea18d
Artem <artemb@kingd.agency>
2021-02-20 18:54:20 +0200

    starter kit

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Layout = () => {
+  return (
+    <>
+      <h1>header</h1>
+      <h2>left sidebar</h2>
+      <h2>right sidebar</h2>
+    </>
+  );
+};
+
+export default Layout;
