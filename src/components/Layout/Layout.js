6488a52fed21dc4e63ec8a6004234c0f1a18ea4a
Slavik <matveevslavik379@gmail.com>
2021-02-27 16:15:07 +0200

    Update styles for  wrapper
    - add maxwidth for layout
    - add root wrapper background

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,13 +1,20 @@
 import React from 'react';
+import { Box, Typography } from '@material-ui/core';
+
 import AppBar from './components/AppBar';
 
+import useStyles from './useStyles';
+
 const Layout = ({ children }) => {
+  const classes = useStyles();
   return (
-    <>
+    <Box className={classes.root}>
       <AppBar />
-      <h2>left sidebar</h2>
-      {children}
-    </>
+      <Box className={classes.layoutWrapper}>
+        <Typography>left sidebar</Typography>
+        {children}
+      </Box>
+    </Box>
   );
 };
 
---
+++ src/components/Layout/useStyles.js
@@ -0,0 +1,11 @@
+import { makeStyles } from '@material-ui/core';
+
+export default makeStyles((theme) => ({
+  root: {
+    background: '#dee1e5',
+  },
+  layoutWrapper: {
+    maxWidth: 1440,
+    margin: theme.spacing(0, 'auto'),
+  },
+}));
