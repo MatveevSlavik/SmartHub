cfdaf61905430e0b3e18cf0f6ccd932162e1ff3b
Artem <artemb@kingd.agency>
2021-04-16 17:43:34 +0300

    Fix imports

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
 import { Route, Switch, useHistory } from 'react-router-dom';
-import QuestionForm from './components/Layout/QuestionForm/QuestionForm';
+import QuestionForm from './components/QuestionForm';
 import withLayout from './components/Layout/withLayout';
 import LoginForm from './components/LoginForm/LoginForm';
 
-import QuestionPageContent from './components/QuestionPageContent/QuestionPageContent';
+import QuestionPageContent from './components/QuestionPageContent';
 import { login } from './store/actions/authActions';
 
 const App = () => {
@@ -21,7 +21,7 @@ const App = () => {
         push('/');
       })
       .catch((_err) => {
-        dispatch({ type: 'LOGIN_ERROR' });
+        dispatch({ type: 'LOGIN_FAILED' });
       })
       .finally(() => setSubmitting(false));
   };
@@ -39,7 +39,7 @@ const App = () => {
         component={() => <LoginForm handleSubmit={handleSubmitLogin} />}
       />
       <Route
-        path="/createQuestion"
+        path="/create-question"
         exact
         component={() => withLayout(<QuestionForm />)}
       />
